20de39c820a50855f9997d2947c8050e71e6ce05
Songuyen06 <sonnhhe163733@fpt.edu.vn>
2025-02-14 02:10:35 +0700

    add filter doctor by specialty,degree,..be

--- AppointmentSchedulingApp/my-next-app/src/services/doctorService.ts
+++ AppointmentSchedulingApp/my-next-app/src/services/doctorService.ts
@@ -8,10 +8,38 @@ export const doctorService = {
 
     return res.json();
   },
-  async getDoctorDetailById(doctorId:string | number): Promise<IDoctorDetail> {
+  async getDoctorDetailById(doctorId: string | number): Promise<IDoctorDetail> {
     const res = await fetch(`http://localhost:5220/api/Doctors/${doctorId}`, {
-      next: { revalidate: 5   }, 
+      next: { revalidate: 5 },
     });
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+
+    return res.json();
+  },
+  async getDoctorListByFilter(
+    specialties: string[],
+    academicTitles: string[],
+    degrees: string[]
+  ): Promise<IDoctor[]> {
+    let query = [];
+
+    if ( specialties.length > 0) {
+      query.push(
+        `specialtyNames in (${specialties.map((s) => `'${s}'`).join(",")})`
+      );
+    }
+    if (  academicTitles.length > 0) {
+      query.push(
+        `academicTitle in (${academicTitles.map((a) => `'${a}'`).join(",")})`
+      );
+    }
+    if ( degrees.length > 0) {
+      query.push(`degree in (${degrees.map((d) => `'${d}'`).join(",")})`);
+    }
+
+    const res = await fetch( `http://localhost:5220/api/Doctors${ query.length > 0 ? `?$filter=${query.join(" or ")}` : ""}`)
     if (!res.ok) {
       throw new Error(`HTTP error! Status: ${res.status}`);
     }

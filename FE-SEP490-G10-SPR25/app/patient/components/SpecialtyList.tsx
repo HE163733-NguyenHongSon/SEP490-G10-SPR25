38921f374fbe29b4c10517acfe35f1f59c2369d5
NguyenThuong <thuongnvhe140301@fpt.edu.vn>
2025-03-02 21:52:30 +0700

    update specialty list

--- FE-SEP490-G10-SPR25/app/patient/components/SpecialtyList.tsx
+++ FE-SEP490-G10-SPR25/app/patient/components/SpecialtyList.tsx
@@ -1,8 +1,8 @@
 "use client";
-import Image from "next/image";
 import { specialtyService } from "@/services/specialtyService";
-import { useState, useEffect } from "react";
+import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 export const SpecialtyList = () => {
   const [specialtys, setSpecialtys] = useState<ISpecialty[]>([]);
   useEffect(() => {

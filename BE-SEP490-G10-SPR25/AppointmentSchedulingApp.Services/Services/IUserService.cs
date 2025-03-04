f847931ebdac9c55db31688dd287aa67a350ec64
HE163593-BuiMinhManh <132968220+HE163593-BuiMinhManh@users.noreply.github.com>
2025-03-05 03:24:35 +0700

    Update Send Email.cs

--- BE-SEP490-G10-SPR25/AppointmentSchedulingApp.Services/Services/IUserService.cs
+++ BE-SEP490-G10-SPR25/AppointmentSchedulingApp.Services/Services/IUserService.cs
@@ -14,10 +14,8 @@ namespace AppointmentSchedulingApp.Services.Services
     public interface IUserService
     {
         Task<UserDTO?> LoginUser(SignInDTO userLogin, StringBuilder message);
-        //Task<UserDTO?> RegisterUser(RegistrationDTO registrationDto, StringBuilder message);
+        
         string GenerateToken(UserDTO user);
-        //public Task<IdentityResult> SignUpAsync(RegistrationDTO registrationDTO);
-        //public Task<string> SignInAsync(SignInDTO signInDTO);
         Task<IdentityResult?> RegisterPatient(RegistrationDTO registrationDTO);
 
 

48ab1ea3081165e92fd527bb6e7b73a2594f4066
HE161511-DinhQuangTung <134306261+HE161511-DinhQuangTung@users.noreply.github.com>
2025-04-01 21:49:26 +0700

    Add files via upload

---
+++ AppointmentSchedulingApp.Presentation/Controllers/ReservationsController.cs
@@ -0,0 +1,37 @@
+﻿using AppointmentSchedulingApp.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using AppointmentSchedulingApp.Application.IServices;
+
+namespace AppointmentSchedulingApp.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private IReservationService reservationService;
+
+        public ReservationsController(IReservationService reservationService)
+        {
+            this.reservationService = reservationService;
+        }
+
+        [HttpGet()]
+        [EnableQuery]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await reservationService.GetListReservation());
+        }
+
+        [HttpGet("{status}/{sortBy}")]
+        public async Task<IActionResult> GetListReservationByStatusAndSort( string? status = "pending", string? sortBy = "price_asc")
+        {
+            var reservations = await reservationService.GetListReservationByStatusAndSort(status, sortBy);
+            return Ok(reservations);
+        }
+
+
+
+    }
+}
---
+++ AppointmentSchedulingApp.Presentation/Controllers/SpecialtiesController.cs
@@ -0,0 +1,33 @@
+﻿using AppointmentSchedulingApp.Application.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+
+namespace AppointmentSchedulingApp.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpecialtiesController : ControllerBase
+    {
+        public ISpecialtyService specialtyService { get; set; }
+        public SpecialtiesController(ISpecialtyService specialtyService)
+        {
+            this.specialtyService = specialtyService;
+        }
+        [HttpGet]
+        [EnableQuery]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await specialtyService.GetSpecialtyList());
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSpecialtyById(int id)
+        {
+            var specialtyDetail = await specialtyService.GetSpecialtyDetailById(id);
+            if (specialtyDetail == null) { 
+                return NotFound(); 
+            }
+            return Ok(specialtyDetail);
+        }
+    }
+}
